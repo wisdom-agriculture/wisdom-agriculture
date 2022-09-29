16c722e95582540c2f72a5fedeade25cab318ab8
sh4d0w1ess <miracle@stud.tjut.edu.cn>
2022-09-29 08:20:21 +0800

    feat/push new code

--- CropRecommend/wsgi.py
+++ CropRecommend/wsgi.py
@@ -6,21 +6,9 @@ application = get_wsgi_application()
 # ML registry
 import inspect
 from apps.ml.registry import MLRegistry
-from apps.ml.crop_recommend.clf import RandomForestClassifier
 
 try:
     registry = MLRegistry()
-
-    rf = RandomForestClassifier()
-
-    registry.add_algorithm(endpoint_name="crop_pred",
-                            algorithm_object=rf,
-                            algorithm_name="random forest",
-                            algorithm_status="production",
-                            algorithm_version="0.0.1",
-                            owner="Aeliqwq",
-                            algorithm_description="Random Forest with simple pre- and post-processing",
-                            algorithm_code=inspect.getsource(RandomForestClassifier))
 
 except Exception as e:
     print("Exception while loading the algorithms to the registry,", str(e))
